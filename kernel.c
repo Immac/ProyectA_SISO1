3cdfa034ab17bdb78d7ecd7b48e4c012bf884c38
Immac <immac.gm@gmail.com>
2015-05-09 19:02:03 -0600

    Cleaned kernel.c a little bit

--- kernel.c
+++ kernel.c
@@ -1,15 +1,17 @@
 #define ROWS 80
 #define COLS 25
 #define DISPLAY_CHAR_SIZE 2
-#define VIDEO_ADDRESS_B 0x8000
+#define VIDEO_ADDRESS_BASE 0x8000
 #define VIDEO_ADDRESS_SEGMENT 0xB000
+#define MAX_BIT_VALUE 0xF
 #define WHITE 7
 #define BLACK 0
+
 
 extern void putInMemory (int segment,int address,int character);
 extern int computePosition(int xCoordinate, int yCoordinate);
 extern void loop(void);
-extern void print_string(char *string,int color,int xCoordinate, int yCoordinate);
+extern void printString(char *string,int color,int xCoordinate, int yCoordinate);
 extern void clearScreen(void);
 
 void main() {
@@ -22,8 +24,8 @@ void main() {
   
   clearScreen();
   position = computePosition(x,y);
-  for(i = 0; i < 8; i++){
-      print_string(my_string,i,x,y);
+  for(i = 0; i < MAX_BIT_VALUE; i++){
+      printString(my_string,i,x,y);
       y++;
   }
   loop();
@@ -34,17 +36,16 @@ int computePosition(int xCoordinate,int yCoordinate) {
 }
 
 void loop(void) {
-    while(1) {
-    }
+    while(1);
 }
 
-void print_string(char *string,int color,int xCoordinate,int yCoordinate){
+void printString(char *string,int color,int xCoordinate,int yCoordinate){
     int position = computePosition(xCoordinate,yCoordinate);
     int i = 0;    
     while (string[i] != '\0') {
-	putInMemory(VIDEO_ADDRESS_SEGMENT,VIDEO_ADDRESS_B + position,string[i]);
+	putInMemory(VIDEO_ADDRESS_SEGMENT,VIDEO_ADDRESS_BASE + position,string[i]);
 	position++;
-	putInMemory(VIDEO_ADDRESS_SEGMENT,VIDEO_ADDRESS_B + position,color);
+	putInMemory(VIDEO_ADDRESS_SEGMENT,VIDEO_ADDRESS_BASE + position,color);
 	position++;
 	i++;
     }
@@ -56,7 +57,7 @@ void clearScreen(void) {
     char *empty = " ";
     for (i = 0 ; i < ROWS ; i++ ) {
 	for (j = 0; j < COLS ; j++ ) {
-	    print_string(empty,0,i,j);
+	    printString(empty,0,i,j);
 	}
     }
 }
