cb7edbe0c0a5df57d86a501822e9e8a6b9601448
Immac <immac.gm@gmail.com>
2015-05-12 21:39:14 -0600

    printstring in C working.

--- src/kernel.c
+++ src/kernel.c
@@ -1,18 +1,22 @@
 #include "screenutils.h"
 
 extern void hang(void);
+extern void printString(char *str);
 
 void main() {
-  char chr = 'a';
-  printChar(chr);
-  printChar(chr);
-  printChar(chr);
-  printChar(chr);
-  clearScreen();
-  printChar(chr);
-  hang();
+	char *str = "Hello";
+	printString(str);
+	hang();
 }
 
 void hang(void) {
     while(1);
+}
+
+void printString(char *str) {
+	int i = 0;
+	while(str[i] != '\0') {
+		printChar(str[i]);
+		i++;
+	}
 }
